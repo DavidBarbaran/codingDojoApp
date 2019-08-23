7ec93580fceef86edff64eb25113dd12de9a6a65
Jorge Luis Villavicencio Correa <lacunacoil18@hotmail.com>
2019-08-23 13:35:03 -0500

    impl

--- app/src/main/java/com/coding/dojo/feature/login/LoginActivity.kt
+++ app/src/main/java/com/coding/dojo/feature/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.coding.dojo.feature.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.coding.dojo.R
+import kotlinx.android.synthetic.main.activity_login.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LoginActivity : AppCompatActivity() {
@@ -11,6 +12,18 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        init()
+    }
+
+    private fun init() {
+        btnIngresar.setOnClickListener {
+
+        }
+
+        btnQuieroRegistrarme.setOnClickListener {
+
+        }
     }
 }
